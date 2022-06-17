90ae7647a5152c5e7f4df356c8aa7d208a24cfcb
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-17 16:02:51 +0700

    Update

--- src/config/db.js
+++ src/config/db.js
@@ -7,5 +7,8 @@ const db = new Pool({
       rejectUnauthorized: false
     }
 });
-
+// const db = new Pool({
+//     connectionString: process.env.DATABASE_URL || 'postgresql://billhikmah:bill21@localhost:5432/billhikmah',
+//     ssl: process.env.DATABASE_URL ? true : false
+// });
 module.exports = db;
