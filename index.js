cacb278412269025bd164ef0bcb7c10dee4a9031
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-18 01:06:32 +0700

    update

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ db.connect()
     server.use(
         logger(":method :url :status :res[content-length] - :response-time ms"));
     const corsOptions = {
-        origin: ["http://localhost:3000", "https://starbills.netlify.app", "https://starbills.herokuapp.com", "Access-Control-Allow-Origin"],
+        origin: ["http://localhost:3000", "https://starbills.netlify.app"],
         methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-        allowedHeaders: ["COntent-Type", "Authorization", "x-access-token"]
+        allowedHeaders: ["Content-Type", "Authorization", "x-access-token"]
     };
     server.use("*", cors(corsOptions));
     // server.use(bodyParser.urlencoded({extended:false}));
