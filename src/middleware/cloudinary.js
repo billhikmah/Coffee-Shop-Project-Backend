9da23505fdba9133bece8bd7e362bc3b94c8ce53
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-20 20:16:01 +0700

    Update

--- src/middleware/cloudinary.js
+++ src/middleware/cloudinary.js
@@ -1,12 +1,12 @@
 const {errorResponse, successResponse} = require("../helpers/response");
 const cloudinary = require("../utils/cloudinary");
 
-const uploadPicture = (req, res, next) => {
+const uploadPicture = async (req, res, next) => {
     const picture = req.file;
     console.log(picture);
     if(picture){
         try {
-            const uploadedResponse = cloudinary.uploader.upload(picture, {
+            const uploadedResponse = await cloudinary.uploader.upload(picture, {
                 upload_preset: "mf_default"
             });
             // return successResponse(res, 200, uploadedResponse);
