37ce07ce108c36861d7315611bd9130ef512820b
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-06-21 04:49:02 +0700

    Update

--- src/middleware/cloudinary.js
+++ src/middleware/cloudinary.js
@@ -4,16 +4,14 @@ const cloudinary = require("../utils/cloudinary");
 const uploadPicture = async (req, res, next) => {
     if(req.file){
         const picture = req.file.path;
-        if(picture){
-            try {
-                const response = await cloudinary.uploader.upload(picture, {folder: "public"});
-                console.log(response);
-                req.url = response.url;
-                return next();
-            } catch (error) {
-                console.log("error:", error);
-                errorResponse(res, 500, error);
-            }
+        try {
+            const response = await cloudinary.uploader.upload(picture, {folder: "public"});
+            console.log(response);
+            req.url = response.url;
+            return next();
+        } catch (error) {
+            console.log("error:", error);
+            errorResponse(res, 500, error);
         }
     }
 };
