04101006b7f770737fe058995c11a65c4041f641
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-10 21:44:42 +0700

    update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,12 +2,9 @@ const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 
 cloudinary.config({
-    // cloud_name: process.env.CLOUD_NAME,
-    // api_key: process.env.CLOUDINARY_API_KEY,
-    // api_secret: process.env.CLOUDINARY_API_SECRET
-    cloud_name: "starbillscloud",
-    api_key: "763464258547458",
-    api_secret: "QroR3nxJZCb0ChRNQByDdOV3Hwc"
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
