1d6a44e8d4faf2e9a091741f33255b9ad3e39740
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-19 01:01:32 +0700

    Update config

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,16 @@
 const psql = require("pg");
 const { Pool } = psql;
 
-// const db = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-// });
 const db = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgresql://billhikmah:bill21@localhost:5432/billhikmah",
-  ssl: process.env.DATABASE_URL ? true : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+// const db = new Pool({
+//   connectionString:
+//     process.env.DATABASE_URL ||
+//     "postgresql://billhikmah:bill21@localhost:5432/billhikmah",
+//   ssl: process.env.DATABASE_URL ? true : false,
+// });
 module.exports = db;
